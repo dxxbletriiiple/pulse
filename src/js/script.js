5f150950a1471b00734c33c8229b27cf64e77a0e
dxxbleriiiple <dxxbleriiiple@gmail.com>
2021-03-30 17:35:22 +0300

    some improvements

--- src/js/script.js
+++ src/js/script.js
@@ -30,4 +30,46 @@ $(document).ready(function() {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanx, #order').fadeOut('slow');
+    });
+
+    $('.button__mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+    $('#consultation-form').validate();
+    $('#consultation form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlenght: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите своё имя",
+                minlength: jQuery.validator.format("Имя должно содержать {0} и более символов!")
+            },
+            phone: "Пожалуйста, введите свой номер телефона.",
+            email: {
+                required: "Пожалуйста, введите свой адресс почты.",
+                email: "Неправильно введен адресс почты"
+            }
+        }
+    });
+    $('#order form').validate();
 });
